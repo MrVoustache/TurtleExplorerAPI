fa18ff88e01e2d7e7b44b4df871e1a3d60867876
Vincent Raulin <vincent.raulin.n7@gmail.com>
2026-03-28 12:13:49 +0100

    Added autocompletion for locations

---
+++ autocomplete/locations.lua
@@ -0,0 +1,22 @@
+--- Handles the autocompletion of the arguments of the locations script.
+--- 
+local tracker = require "tracker"
+
+
+
+
+
+local function complete(shell, arg_index, current_arg, previous_args)
+    if arg_index == 1 then
+        return textutils.complete(current_arg, {"list", "ls", "remove", "rm", "delete", "del", "add", "save"})
+    else
+        local mode = previous_args[1]
+        if (mode == "remove" or mode == "rm" or mode == "delete" or mode == "del") and arg_index == 2 then
+            return textutils.complete(current_arg, tracker.get_dict_names())
+        else
+            return {}
+        end
+    end
+end
+
+return complete
