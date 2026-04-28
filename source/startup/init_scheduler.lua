a9c6fb7cf948c40357f4f65fea4d9fa4497adafd
Vincent Raulin <vincent.raulin.n7@gmail.com>
2026-04-28 23:23:15 +0200

    Fixed small path typo

--- source/startup/init_scheduler.lua
+++ source/startup/init_scheduler.lua
@@ -1,4 +1,4 @@
 shell.setPath(shell.path() .. ":/lib/turtle_explorer_api/bin")
-for index, file in ipairs(fs.list("/lib/turtle_explorer_api/autocompletion")) do
-    shell.run("/lib/turtle_explorer_api/autocompletion/"..file)
+for index, file in ipairs(fs.list("/lib/turtle_explorer_api/autocomplete")) do
+    shell.run("/lib/turtle_explorer_api/autocomplete/"..file)
 end
